d5bb619a23dc6656c17078e822a1c988bdea7370
Pablo Vega <pablo.vega@tiendeo.comm>
2019-07-20 21:10:02 +0200

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import './index.css';
+import './normalize.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
