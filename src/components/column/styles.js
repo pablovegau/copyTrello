08c48aaec3425e2f7a554d25de27a357e02f2614
Pablo Vega <pablo.vega@tiendeo.comm>
2019-07-21 12:06:50 +0200

    Add column button to add new Taks

--- src/components/column/styles.js
+++ src/components/column/styles.js
@@ -1,25 +1,46 @@
 import styled from 'styled-components';
 import { rem } from 'polished';
 
-import { appMargin, appPadding, appGrayColors } from '../../theme';
+import { appMargin, appPadding, appGrayColors, appColors, appFontSizes } from '../../theme';
 
 const columnWidth = rem('272px');
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: ${columnWidth};
   margin: ${appMargin.milli};
+  width: ${columnWidth};
+  min-width: ${columnWidth};
   border-radius: 4px;
   background-color: ${appGrayColors.black10};
 `;
 
 export const Title = styled.h3`
-  padding: ${appPadding.milli};
+  padding-bottom: ${appPadding.milli};
+  padding-left: ${appPadding.base};
+  padding-top: ${appPadding.base};
+  font-weight: 500;
 `;
 
 export const TaskList = styled.div`
-  padding: ${appPadding.milli};
   flex-grow: 1;
+  padding: ${appPadding.milli};
   min-height: ${rem('100px')};
+`;
+
+export const AddButton = styled.button`
+  padding: ${appPadding.milli};
+  border: none;
+  border-radius: 0 0 4px 4px;
+  background-color: transparent;
+  color: ${appGrayColors.black60};
+  font-size: ${appFontSizes.milli};
+  text-align: left;
+  cursor: pointer;
+  transition: color 0.3s, background-color 0.3s;
+
+  &:hover {
+    background-color: ${appColors.lightPrimary};
+    color: ${appGrayColors.black80};
+  }
 `;
--- src/theme/fontSizes.js
+++ src/theme/fontSizes.js
@@ -1,10 +1,10 @@
 export const appFontSizes = {
-  exa: '2.5rem',
-  peta: '1.875rem', // 30px
-  tera: '1.5rem', // 24px
-  giga: '1.375rem', // 22px
-  mega: '1.25rem', // 20px
-  kilo: '1.125rem', // 18px
-  base: '1rem', // 16px
-  milli: '0.875rem', // 14px
+  exa: '2.5em',
+  peta: '1.875em', // 30px
+  tera: '1.5em', // 24px
+  giga: '1.375em', // 22px
+  mega: '1.25em', // 20px
+  kilo: '1.125em', // 18px
+  base: '1em', // 16px
+  milli: '0.875em', // 14px
 };
